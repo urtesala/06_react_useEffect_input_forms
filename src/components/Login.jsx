3655f5540a24841d0e26f031139eacd1f0ff7c1b
urtesala <urtesaladziute@gmail.com>
2022-11-28 11:21:03 +0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,9 @@ function Login(props) {
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
 
+  // state for errors
+  const [errorValue, setErrorValue] = useState('');
+
   function emailInputHandler(e) {
     setEmailValue(e.target.value);
   }
@@ -14,9 +17,49 @@ function Login(props) {
   // susikurti state email ir passwordui.
   // susieti su two way binding inputus su state
   // atvaizduoti htmle/jsx ivestas reiksmes
+
+  function loginHandler(event) {
+    // stabdyti puslapio perkrovima
+    event.preventDefault();
+    // clear all errors
+    setErrorValue('');
+    console.log('react con trolls the form');
+    // cia mes dirbam su state reiksmem  emailValue ir passwordValue
+    console.log({ emailValue, passwordValue });
+
+    // mini validation: jei neivesta kazkuri reikme tai klaida
+
+    if (emailValue === '' || passwordValue === '') {
+      console.warn('big big problem');
+      // setError
+      setErrorValue('Prasome uzpildyti visus laukus');
+      return;
+    }
+
+    console.log('viskas ciki siunciam forma >>>>>> ');
+  }
+
+  const showError = errorValue !== '';
+
+  function sendLoginReq(loginObj) {
+    const url = 'https://reqres.in/api/login';
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(loginObj),
+    })
+      .then((resp) => resp.json())
+      .then((dataInJs) => {
+        console.log('dataInJs ===', dataInJs);
+      })
+      .catch((err) => console.warn('login error', err));
+  }
+
   return (
     <div>
-      <form className='card'>
+      <form onSubmit={loginHandler} className='card'>
+        {showError && <h3 className='errorAlert'>{errorValue}</h3>}
+
         <input
           onChange={emailInputHandler}
           value={emailValue}
