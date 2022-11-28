f330cf2f4c1886c07beba04c24515135152e5c2d
urtesala <urtesaladziute@gmail.com>
2022-11-28 15:11:35 +0300

    to do done undo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react';
 
 const initTodos = [
   { id: 1, value: 'Buy Eggs', done: false, date: '' },
-  { id: 2, value: 'Go to Shopping', done: true, date: '' },
+  { id: 2, value: 'Go to Shopping', done: true, date: '' }, // 2 el.done = true
   { id: 3, value: 'Do a 100 pushups', done: false, date: '' },
 ];
 const initTodosUpdated = [
   { id: 1, value: 'Buy Eggs', done: false, date: '' },
   { id: 2, value: 'Go to Shopping', done: true, date: '' },
   { id: 3, value: 'Do a 100 pushups', done: false, date: '' },
-  { id: 4, value: 'kas ivesta i input', done: false, date: '' },
 ];
 
 function Todos(props) {
@@ -24,7 +23,7 @@ function Todos(props) {
       id: Math.random().toString().slice(2),
       value: newTodoValue,
       done: false,
-      date: new Date(),
+      date: new Date().toLocaleTimeString(),
     };
     // nekeisdamas orginalo atnaujinti todosArr su todosArr versija turinciam nauja todo
     // negalim keisti newTodoValue reiksmes tiesiogiai su push ar dar kazkuo
@@ -33,6 +32,8 @@ function Todos(props) {
     newTodoArrCopy.push(newTodoObj);
 
     setTodosArr(newTodoArrCopy);
+    // clear out input
+    setNewTodoValue('');
   }
 
   function deleteTodoHandler(idToDelete) {
@@ -46,12 +47,41 @@ function Todos(props) {
     });
   }
 
+  // infered value
+  const noTodosYet = !todosArr.length;
+
+  function handleKey(e) {
+    // console.log('e.code ===', e.code);
+    // console.log('e.keyCode ===', e.keyCode);
+    // jei keyCode === 13 tai paspaustas enter klavisas
+    // iskviesti addNewTodoHandler
+    if (e.keyCode === 13) addNewTodoHandler();
+  }
+
+  function doneTodoHandler(idToBeDoneWith) {
+    console.log('make done el with id', idToBeDoneWith);
+    // veiksmu seka padaryti done
+
+    setTodosArr((prevTodosArr) => {
+      // 3. visa tai padaryti su state copija
+      const stateCopy = [...prevTodosArr];
+      // 1. surasti ta el pagal id
+      const foundTodo = stateCopy.find((tObj) => tObj.id === idToBeDoneWith);
+      console.log('foundTodo ===', foundTodo);
+      // 2. pakeisti jo done i true
+      foundTodo.done = !foundTodo.done;
+      // 4. atnaujinti state su pakeistu done elementu
+      return stateCopy;
+    });
+  }
+
   return (
     <div>
       <fieldset>
         <legend>Add todo</legend>
         <input
           onChange={(e) => setNewTodoValue(e.target.value)}
+          onKeyUp={handleKey}
           value={newTodoValue}
           type='text'
           placeholder='What to do?'
@@ -61,15 +91,24 @@ function Todos(props) {
       </fieldset>
 
       <div className='card'>
-        <h3>Todos</h3>
-        <ol>
-          {todosArr.map((tObj) => (
-            <li key={tObj.id}>
-              {tObj.value}{' '}
-              <button onClick={() => deleteTodoHandler(tObj.id)}>X</button>
-            </li>
-          ))}
-        </ol>
+        {noTodosYet && <h3>No todos yet, add some.</h3>}
+        {!noTodosYet && (
+          <>
+            <h3>Todos</h3>
+            <ol>
+              {todosArr.map((tObj) => (
+                <li key={tObj.id} className={tObj.done ? 'doneTodo' : ''}>
+                  {tObj.value}. created:
+                  {tObj.date}
+                  <button onClick={() => doneTodoHandler(tObj.id)}>
+                    {tObj.done ? 'undo' : 'done'}
+                  </button>
+                  <button onClick={() => deleteTodoHandler(tObj.id)}>X</button>
+                </li>
+              ))}
+            </ol>
+          </>
+        )}
       </div>
     </div>
   );
